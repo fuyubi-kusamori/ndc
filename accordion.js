3945e815a90c86241f97ba4ac9728a4d4f483f79
arietta <165660062+fuyubi-kusamori@users.noreply.github.com>
2024-04-10 10:37:19 +0900

    Add files via upload

---
+++ accordion.js
@@ -0,0 +1,1 @@
+$(function(){$("dd").css("display","none");$("dl dt").click(function(){if($("+dd",this).css("display")=="none"){$("+dd",this).slideDown();}else{$("+dd",this).slideUp();}});});
